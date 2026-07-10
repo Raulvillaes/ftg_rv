40e56c760b03e31cda12778a0dd63514582dcdc6
Raulvillaes <raulvillaes@outlook.com>
2026-07-09 19:23:41 -0500

    fix: elimino imports sin uso en controller.launch.py

--- launch/controller.launch.py
+++ launch/controller.launch.py
@@ -7,7 +7,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
 from launch_ros.actions import Node
 
 
